36f0f9ebfba8925d4bc549351ab10532b7b29cec
Amilharshak <amilvk125@gmail.com>
2023-07-03 19:51:18 +0530

    changed codes in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+
 void main()
 {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget
 {
-@override
+  @override
   Widget build(BuildContext context)
-{
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    // home: LoginPage(),
-  );
-}
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage()
+    );
+  }
 }
